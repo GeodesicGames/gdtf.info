b10aecc3be5711ba9c28c1457c46b28d9a5ecbd6
Peter Klipfel <peter@klipfel.me>
2021-02-01 14:03:56 -0700

    Removing purgecss
    It's removing CSS that is actually used, and I don't have time to debug the whole hugo pipeline to figure out how to make it recognize new docs files. I've left the include in place as a reminder to anyone who might want to fix this in the future

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -5,19 +5,5 @@ const whitelister = require('purgecss-whitelister');
 module.exports = {
   plugins: [
     autoprefixer(),
-    purgecss({
-      content: [
-        './layouts/**/*.html',
-        './content/**/*.md',      
-      ],
-      safelist: [
-        'lazyloaded',
-        ...whitelister([
-          './assets/scss/components/_code.scss',
-          './assets/scss/components/_search.scss',
-          './assets/scss/common/_dark.scss',
-        ]),
-      ],
-    }),
   ],
 }
